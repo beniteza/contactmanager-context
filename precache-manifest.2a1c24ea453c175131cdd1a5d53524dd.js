e513f6e70da5cdc71d1309a8fc6f7fe17f9c0386
Axviel <axviel.benitez@upr.edu>
2018-12-18 22:35:07 -0400

    Updates

---
+++ precache-manifest.2a1c24ea453c175131cdd1a5d53524dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36cde667cad300054bad",
+    "url": "/contactmanager-context/static/js/runtime~main.36cde667.js"
+  },
+  {
+    "revision": "9fba2aed66ad267ee108",
+    "url": "/contactmanager-context/static/js/main.9fba2aed.chunk.js"
+  },
+  {
+    "revision": "588687b2fb1c3d243ef3",
+    "url": "/contactmanager-context/static/js/1.588687b2.chunk.js"
+  },
+  {
+    "revision": "9fba2aed66ad267ee108",
+    "url": "/contactmanager-context/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "588687b2fb1c3d243ef3",
+    "url": "/contactmanager-context/static/css/1.dbc1ef8f.chunk.css"
+  },
+  {
+    "revision": "cd9aa1f2d25a56b6e5cd4532439c22ee",
+    "url": "/contactmanager-context/index.html"
+  }
+];
